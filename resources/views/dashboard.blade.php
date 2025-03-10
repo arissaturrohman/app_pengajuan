e27d6fe45fce987f908aeb753c191cf067efe90a
ARISSTURROHMAN <nangarisku@gmail.com>
2025-03-10 20:46:23 +0700

    Fitur CRUD Pengajuan

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,13 +7,13 @@
 
 <div class="row">
 
-    <div class="col-xl-4 col-md-4 mb-4">
+    <div class="col-xl-3 col-md-3 mb-3">
         <div class="card border-left-primary shadow h-100 py-2">
             <div class="card-body">
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
                         <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                            Earnings (Monthly)</div>
+                            Nasabah</div>
                         <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
                     </div>
                     <div class="col-auto">
@@ -25,13 +25,13 @@
     </div>
 
     <!-- Earnings (Monthly) Card Example -->
-    <div class="col-xl-4 col-md-4 mb-4">
+    <div class="col-xl-3 col-md-3 mb-3">
         <div class="card border-left-success shadow h-100 py-2">
             <div class="card-body">
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
                         <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                            Earnings (Annual)</div>
+                            Kelompok</div>
                         <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
                     </div>
                     <div class="col-auto">
@@ -43,12 +43,30 @@
     </div>
 
     <!-- Earnings (Monthly) Card Example -->
-    <div class="col-xl-4 col-md-4 mb-4">
+    <div class="col-xl-3 col-md-3 mb-3">
+        <div class="card border-left-success shadow h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                            Pengajuan</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Earnings (Monthly) Card Example -->
+    <div class="col-xl-3 col-md-3 mb-3">
         <div class="card border-left-info shadow h-100 py-2">
             <div class="card-body">
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
+                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Realisasi
                         </div>
                         <div class="row no-gutters align-items-center">
                             <div class="col-auto">
